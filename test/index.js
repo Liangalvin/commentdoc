4ee8d36a1adb26b43cf3748911d11da27c40527e
Alvin Liang <liangalvin@yahoo.com>
2015-05-18 00:47:23 -0400

    fixed merge errors

--- test/index.js
+++ test/index.js
@@ -20,33 +20,34 @@ describe('#read', function () {
 		assert.equal(expected, actual);
 	});
 });
-
 describe('#filter', function () {
-    it('should return an array with the file name and only the comments in a file and their line number', function () {
-      var fileName = 'testRead.js',
-          text = read(fileName),
-          filtererdText = filter(fileName, text);
-
-        (filtererdText[0]).should.equal('From testRead.js\n');
-        (filtererdText[1]).should.equal('Line 0: how now');
-    });
-    it('is a function', function(){
-        (typeof filter).should.equal('function');
-    });
+	it('is a function', function () {
+		expect(typeof filter).to.equal('function');
+	});
+	it(
+		'should return an array with the file name and only the comments in a file and their line number',
+		function () {
+			var fileName = 'testRead.js';
+			var text = read(fileName);
+			var filteredText = filter(fileName, text);
+			var expected = ['From testRead.js\n', 'Line 0: how now'];
+			filteredText.should.eql(expected);
+		});
 });
+describe('#write', function () {
+	it('is a function', function () {
+		expect(typeof write).to.equal('function');
+	});
+	it('should write a file with a given array', function () {
+		var fileToRead = 'testRead.js';
+		var text = read(fileToRead);
+		var filteredText = filter(fileToRead, text);
+		var newFile = 'testWrite.md';
+		var expected = 'From testRead.js\n\nLine 0: how now\n';
 
-describe('#write', function() {
-    it('should return the file(with filename) containing only the comments from the original file', function (){
-      var fileName = 'testRead.js',
-          text = read(fileName),
-          filteredText = filter(fileName, text),
-          writeFile = write('testWrite.md', filteredText),
-          newFile = 'testWrite.md',
-          readNewFile = read(newFile);
-
-          (readNewFile).should.equal('From testRead.js\n\nLine 0: how now\n');
-    });
-    it('is a function', function(){
-        (typeof write).should.equal('function');
-    });
+		write(newFile, filteredText);
+		contentsOfNewFile = read(newFile);
+		expect(
+			contentsOfNewFile).to.equal(expected);
+	});
 });
